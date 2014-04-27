4ecad1ddc9e7aac0b0f782a51533b5a437bc8f7b
Jeff Marshall <jeff@classy.co>
2014-04-27 09:54:23 -0400

    The server now processes payment and adds jobs

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var config = require('./config.local');
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var nano = require('nano')('http://'+ config.db_host);
 
+var codeland_db = nano.use('codeland');
 var stripe = require("stripe")(
   config.stripe_sk
 );
@@ -36,24 +38,47 @@ if ('development' == app.get('env')) {
 app.post('/api/pay', function(req, res){
   var token = req.body.token;
 
-  stripe.charges.create({
-    amount: 2000,
-    currency: 'cad',
-    card: token,
-    description: 'job posting',
-    metadata: { username: req.body.username }
-  }, function(error, charge){
-    if (error){ return res.json({ error: error }) }
+  codeland_db.insert(job, function(couchdb_error, couchdb_response){
+    if (couchdb_error) return res.json({
+      couchdb_error: {
+        message: couchdb_error.message
+      }
+    });
 
-    return res.json(charge);
-  });
+    stripe.charges.create({
+      amount: 2000,
+      currency: 'cad',
+      card: token,
+      description: 'job posting',
+      metadata: {
+        username: req.body.username,
+        job_id: couchdb_response.id
+      }
+    }, function(stripe_error, charge){
+      if (stripe_error){
+        codeland_db.destroy(couchdb_response.id, couchdb_response.rev);
+        return res.json({ stripe_error: stripe_error })
+      }
+
+      job._id = couchdb_response.id;
+      job._rev = couchdb_response.rev;
+      job.stripe_payment_id = charge.id;
+      codeland_db.insert(job);
+
+      return res.json({
+        charge: charge,
+        job_id: job._id
+      });
+    });
+  })
 });
 
 app.all('*', function(req, res){
   res.render('index', {
     stripe_pk: config.stripe_pk,
     db_host: config.db_host,
-    db_name: config.db_name
+    db_name: config.db_name,
+    cities: config.cities
   });
 });
 
